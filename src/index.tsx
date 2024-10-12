85496f3b29923fc8fecce6d029b8d5755e37139c
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2024-10-12 13:22:13 +0300

    Add style

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './App.scss';
 
 const App = () => {
-    return <h1>Hello, React with TypeScript!</h1>;
+    return (
+        <div className="container">
+            <h1>Hello, React with TypeScript!</h1>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,40 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.tsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx','.js','.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                use: 'ts-loader',
-                exclude: /node-modules/,
-            },
-            // {
-            //     test: /\.js$/,
-            //     exclude: /node_modules/,
-            //     use: 'babel-loader',
-            // },
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,  // Add a rule for .scss files
+        use: [
+          'style-loader',  // Injects styles into the DOM
+          'css-loader',    // Turns CSS into JS modules
+          'sass-loader',   // Compiles Sass to CSS
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
+      },
     ],
-    devServer: {
-        static: './dist',
-        hot: true,
-        open: true,
-    },
-    mode: 'development',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    hot: true,
+    open: true,
+  },
+  mode: 'development',
 };
